e3bf0691ada251e9777c7526aa304284c17fa755
Damon Cricket <d.a.tsvirko@gmail.com>
2019-08-24 09:32:42 +0200

    First commit.

---
+++ Map/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Damon Cricket on 24.08.2019.
+//  Copyright © 2019 DC. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - MainViewController
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
